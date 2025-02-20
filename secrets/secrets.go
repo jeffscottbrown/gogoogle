b38012ac396914d51bb8bdf974df71b10b51d5c3
Jeff Brown <brownj@objectcomputing.com>
2025-02-19 20:33:10 -0600

    initial project

---
+++ secrets/secrets.go
@@ -0,0 +1,40 @@
+package secrets
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"os"
+
+	secretmanager "cloud.google.com/go/secretmanager/apiv1"
+	secretspb "cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+)
+
+var projectId = os.Getenv("PROJECT_ID")
+
+func RetrieveSecret(secretName string) (string, error) {
+	if projectId == "" {
+		slog.Error("PROJECT_ID environment variable not set")
+		return "", fmt.Errorf("PROJECT_ID environment variable not set")
+	}
+	ctx := context.Background()
+
+	client, err := secretmanager.NewClient(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to create secret manager client: %w", err)
+	}
+	defer client.Close()
+
+	secretResource := fmt.Sprintf("projects/%s/secrets/%s/versions/latest", projectId, secretName)
+
+	req := &secretspb.AccessSecretVersionRequest{
+		Name: secretResource,
+	}
+	result, err := client.AccessSecretVersion(ctx, req)
+	if err != nil {
+		slog.Error("Error retrieving client secret", "secretName", secretName, "error", err)
+		return "", fmt.Errorf("failed to access secret version: %w", err)
+	}
+
+	return string(result.Payload.Data), nil
+}
